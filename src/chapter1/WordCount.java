0793de6376f6c6fddda528fe4deee5a146b773e2
EricSeokgon <leesk55@gmail.com>
2022-02-22 17:51:27 +0900

    워드 카운터 OOP와 함수형 비교

--- src/chapter1/WordCount.java
+++ src/chapter1/WordCount.java
@@ -5,10 +5,22 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class WordCount {
-    private Set<String> NON_WORDS = new HashSet<String>(){{
-        add("the"); add("add"); add("of"); add("to"); add("a");
-        add("i"); add("it"); add("in"); add("or"); add("is");
-        add("d"); add("s"); add("as"); add("so"); add("but");
+    private Set<String> NON_WORDS = new HashSet<String>() {{
+        add("the");
+        add("add");
+        add("of");
+        add("to");
+        add("a");
+        add("i");
+        add("it");
+        add("in");
+        add("or");
+        add("is");
+        add("d");
+        add("s");
+        add("as");
+        add("so");
+        add("but");
         add("be");
     }};
 
@@ -25,6 +37,23 @@ public class WordCount {
                 }
             }
         }
+        return wordMap;
+    }
+
+    private List<String> regexToList(String words, String regex) {
+        List wordList = new ArrayList<>();
+        Matcher m = Pattern.compile(regex).matcher(words);
+        while (m.find())
+            wordList.add(m.group());
+        return wordList;
+    }
+
+    public Map wordFreq2(String words) {
+        TreeMap<String, Integer> wordMap = new TreeMap<>();
+        regexToList(words, "\\w+").stream()
+                .map(w -> w.toLowerCase(Locale.ROOT))
+                .filter(w -> !NON_WORDS.contains(w))
+                .forEach(w -> wordMap.put(w, wordMap.getOrDefault(w, 0) + 1));
         return wordMap;
     }
 }
