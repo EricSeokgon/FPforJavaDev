1854ffb8d5c09e80d8fa0e139cafef02e566399c
EricSeokgon <leesk55@gmail.com>
2022-02-23 17:18:34 +0900

    회사프로세스의 자바8버전

---
+++ src/chapter2/Process.java
@@ -0,0 +1,18 @@
+package chapter2;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Process {
+    public String cleanNames(List<String> names) {
+        if (names == null) return "";
+        return names.stream()
+                .filter(name -> name.length() > 1)
+                .map(name -> capitalize(name))
+                .collect(Collectors.joining(","));
+    }
+
+    private String capitalize(String e) {
+        return e.substring(0, 1).toUpperCase() + e.substring(1, e.length());
+    }
+}
