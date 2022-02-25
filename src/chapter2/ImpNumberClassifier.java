11814c0a8ad3057af7db9cebd8a333c77542c825
EricSeokgon <leesk55@gmail.com>
2022-02-25 14:23:06 +0900

    최적화된 자연수 분류기

---
+++ src/chapter2/ImpNumberClassifier.java
@@ -0,0 +1,63 @@
+package chapter2;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class ImpNumberClassifier {
+    private int _number;
+    private Map<Integer, Integer> _cache;
+
+    public ImpNumberClassifier(int targetNumber) {
+        _number = targetNumber;
+        _cache = new HashMap<>();
+    }
+
+    private boolean isFactor(int candidate) {
+        return _number % candidate == 0;
+    }
+
+    private Set<Integer> getFactors() {
+        Set<Integer> factors = new HashSet<>();
+        factors.add(1);
+        factors.add(_number);
+        for (int i = 2; i <= Math.sqrt(_number); i++) {
+            if (isFactor(i)) {
+                factors.add(i);
+                factors.add(_number / i);
+            }
+        }
+        return factors;
+    }
+
+    private int aliquotSum() {
+        int sum = 0;
+        for (int i : getFactors()) {
+            sum += i;
+        }
+        return sum - _number;
+    }
+
+    private int cachedAliquotSum() {
+        if (_cache.containsKey(_number)) {
+            return _cache.get(_number);
+        } else {
+            int sum = aliquotSum();
+            _cache.put(_number, sum);
+            return sum;
+        }
+    }
+
+    public boolean isPerfect() {
+        return cachedAliquotSum() == _number;
+    }
+
+    public boolean isAbundant() {
+        return cachedAliquotSum() > +_number;
+    }
+
+    public boolean isDeficient() {
+        return cachedAliquotSum() < _number;
+    }
+}
