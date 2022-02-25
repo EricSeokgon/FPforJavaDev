6875ce1aaea426523e884c09bff1eb92693c3f00
EricSeokgon <leesk55@gmail.com>
2022-02-25 13:35:05 +0900

    자바 8을 사용한 자연수 분류기

--- src/chapter2/NumberClassifier.java
+++ src/chapter2/NumberClassifier.java
@@ -1,12 +1,11 @@
 package chapter2;
 
-import java.util.Collection;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.stream.IntStream;
+
+import static java.util.stream.IntStream.range;
 
 public class NumberClassifier {
-    public static boolean isFactor(final int candidate, final int number) {
+    /*public static boolean isFactor(final int candidate, final int number) {
         return number % candidate == 0;
     }
 
@@ -29,18 +28,27 @@ public class NumberClassifier {
             sum += n;
         }
         return sum - targetNumer;
+    }*/
+
+    public static IntStream factorsOf(int number) {
+        return range(1, number + 1)
+                .filter(potential -> number % potential == 0);
+    }
+
+    public static int aliquotSum(int number) {
+        return factorsOf(number).sum() - number;
     }
 
     public static boolean isPerfect(final int number) {
-        return aliquotSum(factors(number)) == number;
+        return aliquotSum(number) == number;
     }
 
     public static boolean isAbundant(final int number) {
-        return aliquotSum(factors(number)) > number;
+        return aliquotSum(number) > number;
     }
 
     public static boolean isDeficient(final int number) {
-        return aliquotSum(factors(number)) < number;
+        return aliquotSum(number) < number;
     }
 
 }
